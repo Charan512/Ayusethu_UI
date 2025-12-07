5feb16facf66abe385d422792e3a7813d88864e7
AnushaUmmidi <anushaummidi246@gmail.com>
2025-12-07 23:20:11 +0530

    Admin added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login.jsx";
 import Labtest from"./pages/Labtest.jsx";
 // import Farmerdashboard from"./pages/Farmerdashboard.jsx";
 import Collector from"./pages/Collector.jsx";
-// import Admin from "./pages/Admin.jsx"
+import Admin from "./pages/Admin.jsx"
 // import User from "./pages/User.jsx";
 // import Manufacturer from "./pages/Manufacturer.jsx";
 
@@ -18,10 +18,10 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/Labtest" element={<Labtest/>}/> 
         {/* <Route path="/Farmerdashboard" element={<Farmerdashboard/>}/> */}
-        <Route path="/Manufacturer" element ={<Manufacturer/>}/>
+        {/* <Route path="/Manufacturer" element ={<Manufacturer/>}/> */}
         <Route path="/Collector" element={<Collector/>}/>
         <Route path="/Admin" element={<Admin/>}/>
-        <Route path="/User" element={<User/>}/>
+        {/* <Route path="/User" element={<User/>}/> */}
       </Routes>
     </BrowserRouter>
   );
