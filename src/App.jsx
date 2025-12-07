c7ca7f21ae9de10c1fb8835a7d50511b3a23750b
AnushaUmmidi <anushaummidi246@gmail.com>
2025-12-07 23:15:59 +0530

    Collector added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Landing from "./pages/Landing.jsx";
 import Login from "./pages/Login.jsx";
 import Labtest from"./pages/Labtest.jsx";
 // import Farmerdashboard from"./pages/Farmerdashboard.jsx";
-// import Collector from"./pages/Collector.jsx";
+import Collector from"./pages/Collector.jsx";
 // import Admin from "./pages/Admin.jsx"
 // import User from "./pages/User.jsx";
-import Manufacturer from "./pages/Manufacturer.jsx";
+// import Manufacturer from "./pages/Manufacturer.jsx";
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
         <Route path="/Labtest" element={<Labtest/>}/> 
         {/* <Route path="/Farmerdashboard" element={<Farmerdashboard/>}/> */}
         <Route path="/Manufacturer" element ={<Manufacturer/>}/>
-        {/* <Route path="/Collector" element={<Collector/>}/> */}
-        {/* <Route path="/Admin" element={<Admin/>}/> */}
-        {/* <Route path="/User" element={<User/>}/> */}
+        <Route path="/Collector" element={<Collector/>}/>
+        <Route path="/Admin" element={<Admin/>}/>
+        <Route path="/User" element={<User/>}/>
       </Routes>
     </BrowserRouter>
   );
